cd21c5996128e6db5bfc385484c4bb78d2c8e2ef
Nicola Dimatteo <606982+webnicola@users.noreply.github.com>
2025-11-17 11:22:18 +0100

    js in funzione anonima no conflitti

--- web/themes/custom/miotema/js/script.js
+++ web/themes/custom/miotema/js/script.js
@@ -2,10 +2,13 @@
  * API JS Drupal: https://www.drupal.org/docs/drupal-apis/javascript-api/javascript-api-overview
  */
 console.log('No JS qua');
-Drupal.behaviors.ciao = {
-  attach: function (context, settings) {
-    once('ciao', 'body').forEach(function (element) {
-      console.log('Si JS qua', element);
-    })
+
+(function (Drupal) {
+  Drupal.behaviors.ciao = {
+    attach: function (context, settings) {
+      once('ciao', 'body').forEach(function (element) {
+        console.log('Si JS qua in funzione anonima', element);
+      })
+    }
   }
-}
+})(Drupal);
